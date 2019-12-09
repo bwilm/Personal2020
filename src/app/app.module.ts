61dcb46b655454a7be18a2922a7c4fe7de141d9c
bwilm <32461222+bwilm@users.noreply.github.com>
2019-12-08 23:26:38 -0500

    ADDED bio and projects components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeroComponent } from './hero/hero.component';
+import { BioComponent } from './bio/bio.component';
+import { NavComponent } from './nav/nav.component';
+import { ProjectsComponent } from './projects/projects.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroComponent,
+    BioComponent,
+    NavComponent,
+    ProjectsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
